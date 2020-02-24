6971320ceb3609691026a333a9cadba53942ebbe
yasminGhazy <jesygzaki@gmail.com>
2020-02-24 23:59:16 +0200

    yt

--- js/js.js
+++ js/js.js
@@ -1,6 +1,17 @@
 function openNav(){
     document.getElementById("Sidenav").style.width="200px";
+    document.getElementById("main").style.width ="1150px";
+    document.getElementById("main").style.left ="100px";
+    document.getElementById("main").style.right ="0px";
+
+
 } 
 function closeNav(){
     document.getElementById("Sidenav").style.width="0px";
+    document.getElementById("main").style.width="1250px";
+    document.getElementById("main").style.left ="0px";
+    document.getElementById("main").style.right ="100px";
+
+
+
 } 
