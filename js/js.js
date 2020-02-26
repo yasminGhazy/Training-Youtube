0c971bbcd3119050d69af05b889e7f8843259e86
yasminGhazy <jesygzaki@gmail.com>
2020-02-26 21:06:48 +0200

    yt

--- js/js.js
+++ js/js.js
@@ -1,17 +1,19 @@
-function openNav(){
+function Nav(){
+    if(document.getElementById("Sidenav").style.width!="200px"){
     document.getElementById("Sidenav").style.width="200px";
     document.getElementById("main").style.width ="1150px";
     document.getElementById("main").style.left ="100px";
     document.getElementById("main").style.right ="0px";
-
-
-} 
-function closeNav(){
+    document.getElementById("Sidenav").style.zIndex="1";
+    document.getElementById("Sidenav").style.position="fixed";
+    
+    }
+else
+{
     document.getElementById("Sidenav").style.width="0px";
     document.getElementById("main").style.width="1250px";
     document.getElementById("main").style.left ="0px";
     document.getElementById("main").style.right ="100px";
+}
 
-
-
-} 
+}
