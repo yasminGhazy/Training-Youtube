a6602f4ac37fac4c2b8e70248a2dc7c3df077967
yasminGhazy <jesygzaki@gmail.com>
2020-02-28 18:40:33 +0200

    yt

--- js/js.js
+++ js/js.js
@@ -1,19 +1,16 @@
 function Nav(){
-    if(document.getElementById("Sidenav").style.width!="200px"){
-    document.getElementById("Sidenav").style.width="200px";
-    document.getElementById("main").style.width ="1150px";
-    document.getElementById("main").style.left ="100px";
-    document.getElementById("main").style.right ="0px";
-    document.getElementById("Sidenav").style.zIndex="1";
-    document.getElementById("Sidenav").style.position="fixed";
-    
+    if(document.getElementById("main").style.width !="calc(100% - 210px)"){
+        document.getElementById("sidenav").style.width="200px";
+        document.getElementById("main").style.width ="calc(100% - 210px)";
+        document.getElementById("main").style.marginLeft  = "210px";
     }
-else
-{
-    document.getElementById("Sidenav").style.width="0px";
-    document.getElementById("main").style.width="1250px";
-    document.getElementById("main").style.left ="0px";
-    document.getElementById("main").style.right ="100px";
-}
+    else 
+    {
+        document.getElementById("sidenav").style.width="0px";
+        document.getElementById("main").style.width ="calc(100% - 70px)";
+        document.getElementById("main").style.marginLeft  = "70px";
+    }
+    
+
 
 }
