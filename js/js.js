d60664dd4ed63a683fd64106eff7620da4e4c07d
yasminGhazy <jesygzaki@gmail.com>
2020-02-28 22:06:27 +0200

    yt

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@ function Nav(){
     if(document.getElementById("main").style.width !="calc(100% - 210px)"){
         document.getElementById("sidenav").style.width="200px";
         document.getElementById("main").style.width ="calc(100% - 210px)";
-        document.getElementById("main").style.marginLeft  = "210px";
+        document.getElementById("main").style.marginLeft  = "200px";
     }
     else 
     {
